d021527e6e21bed0e9e521e64d3fec3a6895d6f5
kngm <56785259+kengoma@users.noreply.github.com>
2023-07-13 14:51:23 +0900

    ・Vuetify削除 ・Element Plus導入

--- frontend/src/components/RecipeForm.vue
+++ frontend/src/components/RecipeForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import axios from "axios";
 //import { useRouter } from 'vue-router'
 
@@ -7,6 +7,7 @@ import axios from "axios";
 
 const data = reactive({
     responses: "",
+    input: "",
 });
 
 // 疎通確認用
@@ -16,14 +17,37 @@ const requestApi = async () => {
     data.responses = result.data;
 };
 
-
 </script>
 
 <template>
+    <el-card shadow="never" class="w-full max-w-md" :body-style="{ padding: 20 }">
+        <div class="m-8 text-center">
+            <el-link href="https://element-plus.org/en-US/" :underline="false" class="m-0">
+                <img src="https://element-plus.org/images/element-plus-logo.svg" class="block w-[120px] h-[28px]"
+                    alt="Element Plus" />
+            </el-link>
+            <h2 class="my-3">DevChatRecipe</h2>
+        </div>
+        <el-form label-position="top">
+            <el-form-item label="作りたいアプリ概要を入力*">
+                <el-input v-model="data.input" placeholder="Please input" size="large" />
+            </el-form-item>
+            <el-form-item label="言語を入力">
+                <el-input size="large" />
+            </el-form-item>
+            <div class="flex justify-between mb-2">
+                <el-checkbox v-model="checked" label="DBを使用する" size="large" />
+            </div>
+            <div class="flex justify-between mb-2">
+                <el-checkbox v-model="checked" label="クラウドを使用する" size="large" />
+            </div>
+            <el-button @click="requestApi" type="primary" size="large" class="w-full">作成</el-button>
+        </el-form>
+    </el-card>
+
     <div>
-        <h1>{{ data.responses }}</h1>
+        {{ data.responses }}
     </div>
-    <v-btn @click="requestApi">実行</v-btn>
 </template>
 
 <style scoped></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,15 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
-// TODO:一括importしているのでビルドサイズが大きくなる、最終的に必要なコンポーネントのみにしたい
-app.use(createVuetify({
-    components,
-}))
 
 app.mount('#app')
