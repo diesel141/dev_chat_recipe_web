2864be0ec7af7ba141789eaf9682cb377ae801cb
testJcSin <rrrtestjc@gmail.com>
2023-11-15 16:20:15 +0900

    add:ログフォルダがない場合は作成する処理を追加

--- backend/chatgptapi/__init__.py
+++ backend/chatgptapi/__init__.py
@@ -1,4 +1,8 @@
-
 import os
+
 #ログファイルのパス
-LOG_FILE_PATH = os.path.join('./log', 'backend.log') 
+LOG_FILE_PATH = os.path.join('./log', 'backend.log')
+
+# ディレクトリが存在しない場合は作成する
+if not os.path.exists(os.path.dirname(LOG_FILE_PATH)):
+    os.makedirs(os.path.dirname(LOG_FILE_PATH))
