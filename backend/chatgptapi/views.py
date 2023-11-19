6a5e67a45ac28bef8955544193cec7eacb987aed
sirensan <hosinahikaru@gmail.com>
2023-11-19 16:54:49 +0900

    戻し

--- backend/chatgptapi/views.py
+++ backend/chatgptapi/views.py
@@ -6,23 +6,26 @@ from .services.chatgpt import createPrompt
 
 class RequestChatGPTView(APIView):
     def post(self, request, format=None):
-        try:
-            # 質問結果取得
-            content = createPrompt(request)
+        return Response(
+            {"content": "フレームワーク名：React\n開発環境：Visual Studio Code、WebStorm、Atomなどのさまざまなツールを使用してReactを開発できます。開発環境をセットアップするために、node.jsとnpm（ノードパッケージマネージャー）をインストールする必要があります。"}, status=status.HTTP_200_OK
+        )
+        # try:
+        #     # 質問結果取得
+        #     content = createPrompt(request)
             
-            if not content:
-                return Response(
-                    {"content": "結果が取得できませんでした。"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
-            elif content == "max_len_error":
-                return Response(
-                    {"content": "入力項目の桁数が上限を超えています。"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
-            else:
-                return Response(
-                    {"content": content}, status=status.HTTP_200_OK
-                )
-        except Exception as e:
-            # エラーが発生した場合
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        #     if not content:
+        #         return Response(
+        #             {"content": "結果が取得できませんでした。"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        #         )
+        #     elif content == "max_len_error":
+        #         return Response(
+        #             {"content": "入力項目の桁数が上限を超えています。"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        #         )
+        #     else:
+        #         return Response(
+        #             {"content": content}, status=status.HTTP_200_OK
+        #         )
+        # except Exception as e:
+        #     # エラーが発生した場合
+        #     return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
